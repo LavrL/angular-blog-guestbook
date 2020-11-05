56f06bee1ea340cb752d9d23ba5d7f326fdd1a13
LavrL <llembersk@gmail.com>
2020-11-05 22:56:29 +0200

    added filtering for blog posts

--- src/app/blog/blog-post-list/blog-post-list.component.ts
+++ src/app/blog/blog-post-list/blog-post-list.component.ts
@@ -2,6 +2,7 @@ import { BlogService } from '../shared/blog.service';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BlogPost } from '../shared/blog.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog-post-list',
@@ -11,11 +12,26 @@ import { BlogPost } from '../shared/blog.model';
 })
 export class BlogPostListComponent implements OnInit {
   public blogAllPost$: Observable<BlogPost[]>;
+  public fields = ['title', 'author', 'id'];
+  public field = this.fields[0];
+  public value = 'Title: Cats'
 
   constructor(private blogService: BlogService) { }
 
   public ngOnInit(): void {
     this.blogAllPost$ = this.blogService.getAllPosts();
+  }
+
+  public getItems(ev): void {
+    const val = ev.target.value;
+    console.log('value = ', val);
+    if (val && val.trim() !== '') {
+      this.blogAllPost$ = this.blogService.getAllPosts().pipe(
+        map(post => post.filter(p => p[this.field] == val)));
+    }
+    else {
+      this.blogAllPost$ = this.blogService.getAllPosts();
+    }
   }
 
 }
